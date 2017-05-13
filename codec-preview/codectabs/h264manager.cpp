ec7449690e566beae6ee71ed15715f980a26785b
= <gorge.raccoon@gmail.com>
2017-05-13 03:06:47 +0200

    Remove extra functions from codec classes

--- codec-preview/codectabs/h264manager.cpp
+++ codec-preview/codectabs/h264manager.cpp
@@ -4,9 +4,3 @@ H264Manager::H264Manager()
 {
     encodingParameters = "-c:v libx264 -preset ultrafast -f matroska";
 }
-
-void H264Manager::start(QProcess &process, QString file)
-{
-    QString command = "ffmpeg -re -i \"" + file + "\" -c:v libx264 -preset ultrafast -an -f matroska udp://localhost:" + ENCODED_VIDEO_PORT + " -c:v libx264 -preset ultrafast -an -f matroska udp://localhost:"  + VIDEO_PROBE_PORT;
-    process.start(command.toUtf8().constData());
-}
