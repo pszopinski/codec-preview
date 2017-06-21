11e043f31ca97d739efde75f9af4eee181ac5159
= <gorge.raccoon@gmail.com>
2017-06-21 17:06:14 +0200

    Add a stretch to parameters' layout to prevent stretching of labels

--- codec-preview/codectabs/h261manager.cpp
+++ codec-preview/codectabs/h261manager.cpp
@@ -2,12 +2,12 @@
 
 H261Manager::H261Manager(QWidget *parent) : CodecManager(parent, "h261") {
     QList<QString> resolutions({"176:144", "352:288"});
-    addParameter("Resolution", "s", resolutions);
     addParameter("Bitrate", "b:v", "");
     addParameter("Minrate", "minrate", "");
     addParameter("Maxrate", "maxrate", "");
     addParameter("Aspect ratio", "aspect", "");
     addParameter("Qscale", "qscale:v", "");
+    addParameter("Resolution", "s", resolutions);
 }
 
 H261Manager::~H261Manager() {}
