54247d615effbeb12b691573084b3082e449b989
Piotr Puszkarski <piotrpuszk@gmail.com>
2017-12-14 20:10:43 +0100

    qscale

--- codec-preview/codecs/AnyCodec.cpp
+++ codec-preview/codecs/AnyCodec.cpp
@@ -4,4 +4,5 @@ AnyCodec::AnyCodec() {
     addParameter("FPS", "r", "30");
     addParameter("Quantization", "qp", "");
     addCheckBox("Motion vectors", "-vf codecview=mv=pf+bf+bb", true);
+    addParameter("Qscale", "qscale:v", "");
 }
